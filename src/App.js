c268eebaefb4064be3f4a898e21f11d293d49b15
omarandres2803 <omarandres2803@gmail.com>
2021-10-07 00:46:43 +0200

    finished project and deployed it to netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useState } from 'react'
+import React, { Component, Fragment } from 'react'
 import './App.css'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
@@ -9,59 +9,29 @@ import { About } from './components/pages/About'
 import User from './components/users/User'
 
 import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
 
 const App = () => {
-
-  const [alert, setAlert] = useState(null)
-
-  // async componentDidMount(){
-  //   this.setState({loading: true})
-
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-  //   this.setState({users:res.data, loading:false})
-  // }
-
-
-
-  // Get single Github user
-
-
-  // Get user github repos
-
-
-  // Clear users from state
-
-
-
-  // Set Alert
-
-  const showAlert = (msg, type) => {
-    setAlert({msg, type})
-    setTimeout(() => {
-    setAlert(null)
-    }, 5000);
-  }
     return (
       <GithubState>
+        <AlertState>
       <Router>
         <div className='App'>
           <Navbar title="Github Finder" icon="fab fa-github"/>
           <div className="container">
-            <Alert alert={alert} />
+            <Alert />
             <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search  setAlert={showAlert} />
-                  <Users />
-                </Fragment>
-              )} />
+              <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
               <Route exact path='/user/:login/' component={User}/>
+              <Route component={NotFound} />
             </Switch>
           </div>
         </div>
       </Router>
+      </AlertState>
       </GithubState>
 
     )
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,9 +1,12 @@
 import React, { useState, useContext } from 'react'
 import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ( { setAlert } ) => {
+const Search = () => {
 
+    const alertContext = useContext(AlertContext)
+    const {alert} = alertContext
     const githubContext = useContext(GithubContext)
 
     const [text, setText] = useState('')
@@ -15,7 +18,7 @@ const Search = ( { setAlert } ) => {
     const onSubmit = (e) => {
         e.preventDefault()
         if(text === '') {
-            setAlert('Please enter something', 'light')
+            alertContext.setAlert('Please enter something', 'light')
         }else{
             githubContext.searchUsers(text)
             setText('')
@@ -30,11 +33,6 @@ const Search = ( { setAlert } ) => {
             {githubContext.users.length > 0 && <button className="btn btn-light btn-block" onClick={githubContext.clearUsers}>Clear</button> }
             </div>
         )
-}
-
-
-Search.propTypes = {
-    setAlert: PropTypes.func.isRequired
 }
 
 export default Search
